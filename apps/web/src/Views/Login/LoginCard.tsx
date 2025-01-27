2e7991ccfcfb074fd2ee0b02ce3fa3a6a2a6ae37
Krzysztof Kwiatkowski <kestivvi@gmail.com>
2025-01-27 21:18:10 +0100

    feat (apps/web): Split login page

--- apps/web/src/Views/Login/LoginCard.tsx
+++ apps/web/src/Views/Login/LoginCard.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/Button"
 import { Input } from "@/components/ui/Input"
+import Link from "next/link"
 import { useState } from "react"
 
 export const LoginCard = () => {
@@ -15,17 +16,21 @@ export const LoginCard = () => {
     }
 
     return (
-        <div className="bg-black p-4 flex flex-col items-start justify-start gap-4">
-            <Input type="text" placeholder="Login" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <div className="bg-black p-4 flex flex-col items-start justify-start gap-2">
+            <Input type="text" placeholder="Login" value={email} onChange={(e) => setEmail(e.target.value)} className="mb-2" />
             <Input type="password" placeholder="Hasło" value={password} onChange={(e) => setPassword(e.target.value)} />
-
-            {error && <p className="text-red-500 text-sm">{error}</p>}
-
-            <Button onClick={handleLogin}>Zaloguj</Button>
-
             <button className="text-white text-sm">
                 Nie pamiętam hasła
             </button>
+
+            {error && <p className="text-red-500 text-sm">{error}</p>}
+
+            <Button onClick={handleLogin} className="mt-4">Zaloguj</Button>
+
+
+            <Link href="/register" className="text-white text-sm">
+                Nie masz konta? <span className="underline">Zarejestruj się</span>
+            </Link>
         </div>
     )
 }
--- apps/web/src/app/register/page.tsx
+++ apps/web/src/app/register/page.tsx
@@ -1,5 +1,5 @@
 import { jerseyFont } from "@/assets/fonts";
-import { LoginCard } from "@/Views/Login/LoginCard";
+import { RegisterCard } from "@/Views/Login/RegisterCard";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function Page(): JSX.Element {
     return (
         <div className={`${jerseyFont.className} w-full h-full flex flex-col sm:flex-row items-center sm:items-start justify-center gap-16 p-8 mx-auto max-w-[800px]`}>
-            <LoginCard />
+            <RegisterCard />
         </div>
     );
 }
