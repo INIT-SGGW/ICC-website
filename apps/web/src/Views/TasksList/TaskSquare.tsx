a98d26a3cb02c251df2503c9d00f479e124fe047
Krzysztof Kwiatkowski <kestivvi@gmail.com>
2025-01-27 21:19:28 +0100

    feat (apps/web): Task icon forwards to first part

--- apps/web/src/Views/TasksList/TaskSquare.tsx
+++ apps/web/src/Views/TasksList/TaskSquare.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link"
 import Image from "next/image"
+import { useRouter } from "next/navigation";
 
 
 const lock = "/lock.svg"
@@ -14,13 +15,18 @@ export type TaskSquareProps = {
 }
 
 export const TaskSquare = ({ index, unlocked }: TaskSquareProps) => {
-
+    const router = useRouter()
 
     return (
-        <Link key={index} href={`/2025/zadania/${index + 1}`} className="flex flex-col items-center justify-center gap-4 bg-black px-2 py-2 cursor-pointer">
+        <button
+            key={index}
+            onClick={() => router.push(`/2025/${index + 1}/A`)}
+            className={`flex flex-col items-center justify-center gap-4 bg-black px-2 py-2 ${!unlocked ? "cursor-not-allowed" : "cursor-pointer"}`}
+            disabled={!unlocked}
+        >
             <p className={`text-[#FF0000] text-6xl ${!unlocked ? "p-4" : ""}`}>
                 {unlocked ? ROMAN_NUMBERS[index] : <Image src={lock} alt="lock" width={40} height={50} />}
             </p>
-        </Link>
+        </button>
     )
 }
