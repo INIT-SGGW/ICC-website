1eceb90c0d66b325e06e0d0337579e47291b2cae
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2025-01-29 08:45:23 +0100

    fix: delete ui prefix from ui package component's classes

--- packages/ui/src/components/Layout.tsx
+++ packages/ui/src/components/Layout.tsx
@@ -8,23 +8,23 @@ import SGGWLogo from "../assets/sggw-logo.svg";
 
 function TopBar({ adminPanel, navItems }: { adminPanel: boolean, navItems: { href: string, label: string; img?: string }[] }): JSX.Element {
   return (
-    <header className="ui-flex ui-flex-col w-full ui-bg-transparent ui-z-20 ">
-      <div className="ui-flex ui-items-center ui-justify-between ui-w-full ui-px-4 ui-py-6 ui-bg-black ui-text-cred">
-        <a className="ui-flex ui-items-center ui-gap-4" href="/" title="Init Coding Challenge">
-          <img alt="Init Coding Challenge" className="ui-w-24 ui-h-24" src={ICCLogo.src} />
+    <header className="flex flex-col w-full bg-transparent z-20 ">
+      <div className="flex items-center justify-between w-full px-4 py-6 bg-black text-cred">
+        <a className="flex items-center gap-4" href="/" title="Init Coding Challenge">
+          <img alt="Init Coding Challenge" className="w-24 h-24" src={ICCLogo.src} />
           {
             adminPanel ? (
-              <h1 className="ui-text-5xl max-sm:ui-hidden">ICC ADMIN<br />PANEL</h1>
+              <h1 className="text-5xl max-sm:hidden">ICC ADMIN<br />PANEL</h1>
             ) : (
-              <h1 className="ui-text-5xl max-sm:ui-hidden">init CODING<br />CHALLENGE</h1>
+              <h1 className="text-5xl max-sm:hidden">init CODING<br />CHALLENGE</h1>
             )
           }
         </a>
-        <nav className="ui-flex ui-gap-4">
+        <nav className="flex gap-4">
           {navItems.map((item) => (
-            <a className="ui-text-3xl" href={item.href} key={`${item.label}`} title={item.label} >
+            <a className="text-3xl" href={item.href} key={`${item.label}`} title={item.label} >
               {item.img ? (
-                <img alt={item.label} className="ui-w-8 ui-h-8" src={item.img} />
+                <img alt={item.label} className="w-8 h-8" src={item.img} />
               ) : (
                 item.label
               )
@@ -33,7 +33,7 @@ function TopBar({ adminPanel, navItems }: { adminPanel: boolean, navItems: { hre
           ))}
         </nav>
       </div>
-      <div className="ui-h-20 ui-w-full ui-bg-gradient-to-b ui-from-black"></div>
+      <div className="h-20 w-full bg-gradient-to-b from-black"></div>
     </header>
   );
 }
@@ -42,33 +42,33 @@ function Footer(): JSX.Element {
   const date = new Date();
 
   return (
-    <footer className="ui-flex ui-flex-col">
-      <div className="ui-h-20 ui-w-full ui-bg-gradient-to-t ui-from-black"></div>
-      <div className="ui-flex ui-flex-col ui-gap-16 ui-items-center ui-justify-center ui-w-full ui-bg-black ui-text-white ui-px-8 ui-py-12">
-        <div className="ui-flex ui-flex-col ui-gap-4 ui-items-center">
-          <p className="ui-text-xl">Znajdź nas na</p>
-          <div className="ui-flex ui-gap-8">
+    <footer className="flex flex-col">
+      <div className="h-20 w-full bg-gradient-to-t from-black"></div>
+      <div className="flex flex-col gap-16 items-center justify-center w-full bg-black text-white px-8 py-12">
+        <div className="flex flex-col gap-4 items-center">
+          <p className="text-xl">Znajdź nas na</p>
+          <div className="flex gap-8">
             <a href="https://www.instagram.com/kn_init_/" rel="noopener noreferrer" target="_blank" >
-              <img alt="Instagram" className="ui-w-8 ui-h-8" src={InstagramLogo.src} />
+              <img alt="Instagram" className="w-8 h-8" src={InstagramLogo.src} />
             </a>
             <a href="https://www.facebook.com/kninit/" rel="noopener noreferrer" target="_blank" >
-              <img alt="Facebook" className="ui-w-8 ui-h-8" src={FacebookLogo.src} />
+              <img alt="Facebook" className="w-8 h-8" src={FacebookLogo.src} />
             </a>
             <a href="https://www.linkedin.com/company/ko%C5%82o-naukowe-init/about/" rel="noopener noreferrer" target="_blank" >
-              <img alt="Linkedin" className="ui-w-8 ui-h-8" src={LinkedinLogo.src} />
+              <img alt="Linkedin" className="w-8 h-8" src={LinkedinLogo.src} />
             </a>
           </div>
         </div>
-        <div className="ui-flex ui-flex-col ui-gap-4 ui-items-center">
-          <div className="ui-flex ui-gap-8">
+        <div className="flex flex-col gap-4 items-center">
+          <div className="flex gap-8">
             <a href="https://init.wzim.sggw.pl/" rel="noopener noreferrer" target="_blank" >
-              <img alt="KN init" className="ui-w-11 ui-h-11" src={InitLogo.src} />
+              <img alt="KN init" className="w-11 h-11" src={InitLogo.src} />
             </a>
             <a href="https://www.sggw.edu.pl/" rel="noopener noreferrer" target="_blank" >
-              <img alt="SGGW" className="ui-w-11 ui-h-11" src={SGGWLogo.src} />
+              <img alt="SGGW" className="w-11 h-11" src={SGGWLogo.src} />
             </a>
           </div>
-          <p className="ui-text-xl">© {date.getFullYear()} init Coding Challenge</p>
+          <p className="text-xl">© {date.getFullYear()} init Coding Challenge</p>
         </div>
       </div>
     </footer>
@@ -88,9 +88,9 @@ export function RootLayout({
 }): JSX.Element {
   return (
     <html className={font.className} lang="pl" >
-      <body className="ui-flex ui-flex-col ui-min-h-screen">
+      <body className="flex flex-col min-h-screen">
         <TopBar adminPanel={adminPanel} navItems={navItems} />
-        <div className="ui-flex-1 main-content">
+        <div className="flex-1 main-content">
           {children}
         </div>
         <Footer />
