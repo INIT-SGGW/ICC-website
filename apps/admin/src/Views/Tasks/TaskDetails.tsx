6b4a952ea3dce1910133a2d5d22bebc68206ccd7
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2025-03-18 12:55:41 +0100

    fix(apps/admin): remove fake data and use actual release date in TaskDetails component

--- apps/admin/src/Views/Tasks/TaskDetails.tsx
+++ apps/admin/src/Views/Tasks/TaskDetails.tsx
@@ -6,14 +6,6 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import CustomError from "../../utils/CustomError";
 
-const fakeData = {
-    id: 1,
-    title: "Zadania bardzo ważne tralrallalal",
-    releaseDate: new Date(),
-    partA: File,
-    partB: File,
-}
-
 type Props = {
     id: number
 }
@@ -46,7 +38,7 @@ export function TaskDetails({ id }: Props): React.JSX.Element {
                     <>
                         <div >
                             <h1 className="text-cred text-4xl text-center">{data.title}</h1>
-                            <p className="text-white text-2xl text-center">{fakeData.releaseDate.toDateString()}</p>
+                            <p className="text-white text-2xl text-center">{data.releaseDate.toDateString()}</p>
                         </div>
                         <div className="flex flex-wrap sm:flex-nowrap gap-4">
                             <CustomLink type="button" className="text-nowrap px-6" href={`/admin/tasks/update/${id}`}>Modyfikuj zadanie</CustomLink>
