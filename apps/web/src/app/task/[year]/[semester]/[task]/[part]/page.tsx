022afb946d44f5241442915d5d3551dd3ffb8e53
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2025-04-07 13:53:44 +0200

    fix(apps/web): adjust layout properties for better alignment in task page

--- apps/web/src/app/task/[year]/[semester]/[task]/[part]/page.tsx
+++ apps/web/src/app/task/[year]/[semester]/[task]/[part]/page.tsx
@@ -22,9 +22,9 @@ export default function Page({ params }: Props): React.JSX.Element {
             <div className="flex flex-col items-center justify-center gap-4 w-full">
                 <Header year={Number(year)} semester={semester} task={Number(task)} />
 
-                <div className="flex flex-col lg:!flex-row gap-10 w-full">
+                <div className="flex flex-col lg:!flex-row gap-10 w-full items-center lg:!items-start">
                     <Content year={Number(year)} semester={semester} task={Number(task)} part={part} />
-                    <div className="sticky top-8 lg:min-w-[350px] max-w-[400px] h-full">
+                    <div className="sticky top-8 lg:min-w-[350px] max-w-[400px] w-full h-full">
                         <AnswerSection year={Number(year)} semester={semester} task={Number(task)} part={part} />
                     </div>
                 </div>
