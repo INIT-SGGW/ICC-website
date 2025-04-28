bca27a4ac46a2f3f4e7ba059f96b3885221fb366
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2025-04-28 11:04:36 +0200

    feat (apps/web): update getApiKey function

--- apps/admin/src/utils/GetApiKey.ts
+++ apps/admin/src/utils/GetApiKey.ts
@@ -1,7 +1,7 @@
 export function getApiKey(): string {
     switch (process.env.NEXT_PUBLIC_NODE_ENV) {
         case "production":
-            return "0b1cd31926e67d44a01727abcdcdd044f939d1373b16f8daec2e91007faf14f92b3";
+            return process.env.NEXT_PUBLIC_API_KEY_PROD || "";
         case "test":
             return "ApiKey";
         default:
